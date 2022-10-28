4c65cd06410008e81f40837c7edc6f5457e10002
Makaio Bolton <108162816+MakaioB@users.noreply.github.com>
2022-10-28 05:31:57 -1000

    Added the content of Daikichi Path Variables

--- src/main/java/com/makaio/daikichiroutes/DaikichiController.java
+++ src/main/java/com/makaio/daikichiroutes/DaikichiController.java
@@ -1,5 +1,6 @@
 package com.makaio.daikichiroutes;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +20,29 @@ public class DaikichiController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/today/{name}")
+	public String personalToday(@PathVariable("name") String name) {
+		return "You feel very lucky today, " + name + "!";
+	}
+	
+	@RequestMapping("/tomorrow/{name}")
+	public String personalTomorrow(@PathVariable("name") String name) {
+		return name + ", you should be excited for tomorrow! Good things are coming!";
+	}
+	
+	@RequestMapping("/travel/{city}")
+	public String travelToCity(@PathVariable("city") String city) {
+		return "Congratulations! You will soon travel to " + city + "!";
+	}
+	
+	@RequestMapping("/lotto/{number}")
+	public String lotto(@PathVariable("number") int number) {
+		if (number % 2 == 0) {
+			return "You will take a grand journey in the near fucture, but be weary of tempting offers!";
+		} else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends!";			
+		}
 	}
 }
